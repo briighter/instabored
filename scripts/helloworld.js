b7e4b93130c48cf50aa64f3506cddcf34a0d7292
Jason <jaylima0@gmail.com>
2020-05-30 23:33:02 -0400

    Import external project files to GitHub

--- scripts/helloworld.js
+++ scripts/helloworld.js
@@ -3,6 +3,15 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
+let client = new JSO({
+    providerID: "instagram",
+    client_id: "b2285c09b317452785b78df9f5822c87",
+    redirect_uri: "http://your-redirect-uri?code=CODE", // The URL where you is redirected back, and where you perform run the callback() function.
+    authorization: "https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=code",
+    scopes: { request: ["https://www.googleapis.com/auth/userinfo.profile"] }
+})
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
+const options = {
+    auth: {
+        redirectUrl: 'http://localhost:CHANGE-TO-YOUR-PORT/callback',
+        params: {
+            audience: 'https://YOUR_DOMAIN/userinfo',
+            scope: 'openid profile'
+        }
+    }
+};
+const lock = new Auth0Lock('YOUR_CLIENT_ID', 'YOUR_DOMAIN', options);
 
+function signin() {
+    lock.show();
+}
